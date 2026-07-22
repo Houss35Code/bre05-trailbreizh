9555ea3700763a97d85469e68bb02679193473ac
Houss35Code <h.halifa035@gmail.com>
2026-07-22 18:27:59 +0200

    Mise a jour du seeder pour les nouvelles donnees de test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,8 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        \App\Models\User::factory(5)->create(); // garantit qu'il existe des utilisateurs avant le seed des randonnées
+
         $this->call([
             RandonneeSeeder::class,
         ]);
